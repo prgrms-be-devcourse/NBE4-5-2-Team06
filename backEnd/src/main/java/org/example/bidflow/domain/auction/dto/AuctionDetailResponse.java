842052c83a385e6f5f4e1fcb135f53e898197a22
gwony <moonsky1124@naver.com>
2025-03-05 10:52:52 +0900

    ✨ feat: 경매 세부 정보 응답 DTO

---
+++ backEnd/src/main/java/org/example/bidflow/domain/auction/dto/AuctionDetailResponse.java
@@ -0,0 +1,34 @@
+package org.example.bidflow.domain.auction.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.example.bidflow.domain.auction.entity.Auction;
+import org.example.bidflow.domain.product.dto.ProductResponse;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class AuctionDetailResponse {
+
+    private final Long auctionId;
+    private final ProductResponse product;
+    private final Integer startPrice;
+    private final Integer minBid;
+    private final LocalDateTime startTime;
+    private final LocalDateTime endTime;
+    private final String status;
+
+    // 엔티티를 DTO로 변환
+    public static AuctionDetailResponse from(Auction auction) {
+        return AuctionDetailResponse.builder()
+                .auctionId(auction.getAuctionId())
+                .product(ProductResponse.from(auction.getProduct()))      // product DTO 변환
+                .startPrice(auction.getStartPrice())
+                .minBid(auction.getMinBid())
+                .startTime(auction.getStartTime())
+                .endTime(auction.getEndTime())
+                .status(auction.getStatus().name())
+                .build();
+    }
+}
