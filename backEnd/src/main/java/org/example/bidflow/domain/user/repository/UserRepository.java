d0dbb5c708ca7f644876eb43c4c028d3dc2138bd
gwony <moonsky1124@naver.com>
2025-03-06 00:46:05 +0900

    ✨ feat: 유저UUID를 통해 유저 조회

--- backEnd/src/main/java/org/example/bidflow/domain/user/repository/UserRepository.java
+++ backEnd/src/main/java/org/example/bidflow/domain/user/repository/UserRepository.java
@@ -3,8 +3,11 @@ package org.example.bidflow.domain.user.repository;
 import org.example.bidflow.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
+import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmailOrNickname(String email,String nickname);
+
+    Optional<User> findByUserUuid(String userUuid);
 }
