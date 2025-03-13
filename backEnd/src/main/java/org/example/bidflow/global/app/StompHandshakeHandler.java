408436fdff3e1deca323e977dcf92c373ac30a96
gwony <moonsky1124@naver.com>
2025-03-13 10:07:32 +0900

    ✨ feat: 핸드셰이크 핸들러_웹소켓 구독 전 검증

---
+++ backEnd/src/main/java/org/example/bidflow/global/app/StompHandshakeHandler.java
@@ -0,0 +1,62 @@
+package org.example.bidflow.global.app;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.bidflow.global.utils.JwtProvider;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class StompHandshakeHandler implements HandshakeInterceptor {
+
+    private final JwtProvider jwtProvider;
+
+    // Handshake 전 처리
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+        if (!(request instanceof ServletServerHttpRequest servletRequest)) {
+            throw new IllegalArgumentException("WebSocket 요청이 HTTP 요청이 아닙니다.");
+        }
+
+        HttpServletRequest httpRequest = servletRequest.getServletRequest();
+
+        String token = extractToken(httpRequest);
+
+        // WebSocket 세션 속성 저장
+        attributes.put("userUuid", jwtProvider.parseUserUuid(token));
+        attributes.put("nickname", jwtProvider.parseNickname(token));
+
+        log.info("WebSocket Handshake 성공: userUUID={}, nickname={}", jwtProvider.parseUserUuid(token), jwtProvider.parseNickname(token));
+        return true;
+    }
+
+    // Handshake 후 처리
+    @Override
+    public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception){
+        // 추가 로직 필요 시 사용.
+    }
+
+    // Jwt 토큰을 추출하는 메서드
+    private String extractToken(HttpServletRequest request) {
+        String token = request.getHeader("Authorization");
+
+        if (token == null) {
+            token = request.getParameter("token"); // SockJS 대응
+        }
+
+        if (token != null && token.startsWith("Bearer ")) {
+            return token.substring(7); // "Bearer " 제거
+        }
+
+        return token;
+    }
+}
