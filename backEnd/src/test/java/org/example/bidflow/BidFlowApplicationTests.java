9e643b875c80c758f662f1d2adcf05a0bb6b65c6
joungHyeon <jounghyeon123@daum.net>
2025-03-17 15:08:22 +0900

    테스트 주석 처리2

--- backEnd/src/test/java/org/example/bidflow/BidFlowApplicationTests.java
+++ backEnd/src/test/java/org/example/bidflow/BidFlowApplicationTests.java
@@ -1,13 +1,13 @@
-package org.example.bidflow;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BidFlowApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package org.example.bidflow;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BidFlowApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
