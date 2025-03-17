7b629e60bc9a14b6be2a572bb029638d43745811
joungHyeon <jounghyeon123@daum.net>
2025-03-17 10:46:55 +0900

    완료

--- backEnd/src/main/java/org/example/bidflow/global/app/AuctionScheduler.java
+++ backEnd/src/main/java/org/example/bidflow/global/app/AuctionScheduler.java
@@ -61,7 +61,7 @@ public class AuctionScheduler {
                         .user(user)
                         .auction(auction.get())
                         .build();
-//
+
                 winnerRepository.save(winner);
             }
         }
